fb389cde4dea6fb2ab64bfcb9dcb72d92fc49edf
MAAndreev1 <167123315+MAAndreev1@users.noreply.github.com>
2024-07-20 18:09:02 +0500

    Add files via upload

---
+++ m7_hw4_Forms_String.py
@@ -0,0 +1,26 @@
+#  Использование %:
+print('Использование %:')
+team1_num = 5
+team2_num = 6
+print('В команде Мастера кода участников: %s !' % team1_num)
+print('Итого сегодня в командах участников: %s и %s !' % (team1_num, team2_num))
+print()
+
+#  Использование format():
+score_2 = 42
+team1_time = 18015.2
+print('Использование format():')
+print('Команда Волшебники данных решила задач: {} !'.format(score_2))
+print('Волшебники данных решили задачи за {} с !'.format(team1_time))
+print()
+
+# Использование f-строк:
+score_1 = 40
+score_2 = 42
+challenge_result = 'Мастера кода'
+tasks_total = 82
+time_avg = 350.4
+print('Использование f-строк:')
+print(f'Команды решили {score_1} и {score_2} задач.')
+print(f'Результат битвы: победа команды {challenge_result}!')
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.')
